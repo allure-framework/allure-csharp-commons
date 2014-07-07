527a8e9e0f00d11e543729e25b700ab612886750
ilya-murzinov <murz42@gmail.com>
2014-07-07 14:07:47 +0400

    fix versioning

--- AllureCSharpCommons/Properties/AssemblyInfo.cs
+++ AllureCSharpCommons/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using log4net.Config;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.0.1.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
