fdf1773a2f14d2b239cbc517830b3af65c61cdcc
ilya-murzinov <murz42@gmail.com>
2014-07-09 18:12:26 +0400

    comments

--- AllureCSharpCommons/AllureConfig.cs
+++ AllureCSharpCommons/AllureConfig.cs
@@ -2,8 +2,15 @@
 {
     public static class AllureConfig
     {
+        /// <summary>
+        /// False by default.
+        /// If false, empty test suites will not be serialized.
+        /// </summary>
         public static bool AllowEmptySuites { get; set; }
 
+        /// <summary>
+        /// The directory must be created before running tests.
+        /// </summary>
         public static string ResultsPath { get; set; }
     }
 }
