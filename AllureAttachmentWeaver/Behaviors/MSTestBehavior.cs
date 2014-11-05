690444c4b1de268d593beb4170b8c6584fdd2361
Daniel Kugel <mail4dk@gmail.com>
2014-11-05 11:27:03 +0200

    Refactored.

--- AllureAttachmentWeaver/Behaviors/MSTestBehavior.cs
+++ AllureAttachmentWeaver/Behaviors/MSTestBehavior.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using AllureCSharpCommons;
+using AllureCSharpCommons.Utils;
 
 namespace AllureAttachmentWeaver
 {
@@ -10,8 +11,7 @@ namespace AllureAttachmentWeaver
     {
         private AssemblyDefinition mUnitTestingAssembly;
         private TypeReference mTestContextType;
-
-        const string TEXT_MIME_TYPE = "text/plain";
+        private MethodReference mAddResultFileMethodReference = null;
 
         public override void Weave(MethodDefinition method)
         {
@@ -21,7 +21,8 @@ namespace AllureAttachmentWeaver
 
             if (testContextProperty == null)
             {
-                testContextProperty = AddTestContextProperty(method.DeclaringType, "TestContext");
+                TestContextWeaver testContextWeaver = new TestContextWeaver(method.DeclaringType, "TestContext", mTestContextType);
+                testContextProperty = testContextWeaver.AddTestContextProperty();
             }
 
             WriteResultFile(method, testContextProperty.GetMethod);
@@ -36,19 +37,25 @@ namespace AllureAttachmentWeaver
 
             MethodReference writeFile;
 
-            string mimeType = GetAttachmentMimeType(method);
-            if (string.Compare(mimeType, TEXT_MIME_TYPE, true) == 0)
+            string mimeType = GetAttachmentMimeType(method);            
+            string methodName;
+            Type argumentType;
+            
+            if (MimeTypes.IsText(mimeType))
             {
-                writeFile = method.Module.Import(typeof(Attachments).GetMethod("WriteText", new[] { typeof(string) }));
+                methodName = "WriteText";
+                argumentType = typeof(string);
             }
             else
             {
-                writeFile = method.Module.Import(typeof(Attachments).GetMethod("WriteBinary", new[] { typeof(byte[]), typeof(string) }));
-                il.Append(Instruction.Create(OpCodes.Ldstr, mimeType));
+                methodName = "WriteBinary";
+                argumentType = typeof(byte[]);
             }
-
+            
+            writeFile = method.Module.Import(typeof(Attachments).GetMethod(methodName, new[] { argumentType, typeof(string) }));
+            
+            il.Append(Instruction.Create(OpCodes.Ldstr, mimeType));
             il.Append(Instruction.Create(OpCodes.Call, writeFile));
-
             il.Append(Instruction.Create(OpCodes.Stloc, filePath));
 
             il.Append(Instruction.Create(OpCodes.Ldarg_0));
@@ -62,8 +69,13 @@ namespace AllureAttachmentWeaver
 
         private MethodReference GetAddResultFileMethodReference(ModuleDefinition usedModule)
         {
-            TypeDefinition testContextType = mTestContextType.Resolve();
-            return usedModule.Import(testContextType.Methods.First<MethodDefinition>(_ => _.Name == "AddResultFile"));
+            if (mAddResultFileMethodReference == null)
+            {
+                TypeDefinition testContextType = mTestContextType.Resolve();
+                mAddResultFileMethodReference = usedModule.Import(testContextType.Methods.First<MethodDefinition>(_ => _.Name == "AddResultFile"));
+            }
+            
+            return mAddResultFileMethodReference;
         }
 
         private PropertyDefinition GetTestContextProperty(TypeDefinition type)
@@ -77,49 +89,6 @@ namespace AllureAttachmentWeaver
             return null;
         }
 
-        private PropertyDefinition AddTestContextProperty(TypeDefinition type, string name)
-        {
-            PropertyDefinition testContextProperty = new PropertyDefinition(name, PropertyAttributes.None, mTestContextType);
-            FieldDefinition field = AddBackingField(type, name);
-            testContextProperty.GetMethod = AddGetMethod(type, name, field);
-            testContextProperty.SetMethod = AddSetMethod(type, name, field);
-            type.Properties.Add(testContextProperty);
-            return testContextProperty;
-        }
-                
-        private FieldDefinition AddBackingField(TypeDefinition type, string name)
-        {
-            FieldDefinition field = new FieldDefinition("m_" + name + "<>" + DateTime.Now.Ticks, Mono.Cecil.FieldAttributes.Private, mTestContextType);
-            type.Fields.Add(field);
-            return field;
-        }
-
-        private MethodDefinition AddGetMethod(TypeDefinition type, string name, FieldDefinition backingField)
-        {
-            MethodDefinition get = new MethodDefinition("get_" + name, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, mTestContextType);
-            ILProcessor il = get.Body.GetILProcessor();
-            il.Append(Instruction.Create(OpCodes.Ldarg_0));
-            il.Append(Instruction.Create(OpCodes.Ldfld, backingField));
-            il.Append(Instruction.Create(OpCodes.Ret));
-            get.SemanticsAttributes = MethodSemanticsAttributes.Getter;
-            type.Methods.Add(get);
-            return get;
-        }
-
-        private MethodDefinition AddSetMethod(TypeDefinition type, string name, FieldDefinition backingField)
-        {
-            MethodDefinition set = new MethodDefinition("set_" + name, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, type.Module.Import(typeof(void)));
-            ILProcessor il = set.Body.GetILProcessor();
-            il.Append(Instruction.Create(OpCodes.Ldarg_0));
-            il.Append(Instruction.Create(OpCodes.Ldarg_1));
-            il.Append(Instruction.Create(OpCodes.Stfld, backingField));
-            il.Append(Instruction.Create(OpCodes.Ret));
-            set.Parameters.Add(new ParameterDefinition("value", ParameterAttributes.None, mTestContextType));
-            set.SemanticsAttributes = MethodSemanticsAttributes.Setter;
-            type.Methods.Add(set);
-            return set;
-        }
-
         private void LoadUnitTestingAssembly(ModuleDefinition module)
         {
             AssemblyNameReference assemblyNameReference = module.AssemblyReferences.FirstOrDefault<AssemblyNameReference>(_ => _.Name == "Microsoft.VisualStudio.QualityTools.UnitTestFramework");
@@ -128,6 +97,65 @@ namespace AllureAttachmentWeaver
             mUnitTestingAssembly = assemblyResolver.Resolve(assemblyNameReference);
 
             mTestContextType = module.Import(mUnitTestingAssembly.MainModule.GetTypes().First<TypeDefinition>(_ => _.FullName == "Microsoft.VisualStudio.TestTools.UnitTesting.TestContext"));
+        }
+        
+        private class TestContextWeaver
+        {
+            private string mName;
+            private TypeDefinition mTargetType;
+            private FieldDefinition mBackingField;
+            private TypeReference mTestContextType;
+            
+            public TestContextWeaver(TypeDefinition targetType, string name, TypeReference testContextType)
+            {
+                mName = name;   
+                mTargetType = targetType; 
+                mTestContextType = testContextType;
+            }
+            
+            public PropertyDefinition AddTestContextProperty()
+            {
+                PropertyDefinition testContextProperty = new PropertyDefinition(mName, PropertyAttributes.None, mTestContextType);
+                AddBackingField();
+                testContextProperty.GetMethod = AddGetMethod();
+                testContextProperty.SetMethod = AddSetMethod();
+                mTargetType.Properties.Add(testContextProperty);
+                return testContextProperty;
+            }
+
+            private FieldDefinition AddBackingField()
+            {
+                FieldDefinition field = new FieldDefinition("m_" + mName + "<>" + DateTime.Now.Ticks, Mono.Cecil.FieldAttributes.Private, mTestContextType);
+                mTargetType.Fields.Add(field);
+                mBackingField = field;
+                return field;
+            }
+
+            private MethodDefinition AddGetMethod()
+            {
+                MethodDefinition get = new MethodDefinition("get_" + mName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, mTestContextType);
+                ILProcessor il = get.Body.GetILProcessor();
+                il.Append(Instruction.Create(OpCodes.Ldarg_0));
+                il.Append(Instruction.Create(OpCodes.Ldfld, mBackingField));
+                il.Append(Instruction.Create(OpCodes.Ret));
+                get.SemanticsAttributes = MethodSemanticsAttributes.Getter;
+                mTargetType.Methods.Add(get);
+                return get;
+            }
+
+            private MethodDefinition AddSetMethod()
+            {
+                MethodDefinition set = new MethodDefinition("set_" + mName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, mTargetType.Module.Import(typeof(void)));
+                ILProcessor il = set.Body.GetILProcessor();
+                il.Append(Instruction.Create(OpCodes.Ldarg_0));
+                il.Append(Instruction.Create(OpCodes.Ldarg_1));
+                il.Append(Instruction.Create(OpCodes.Stfld, mBackingField));
+                il.Append(Instruction.Create(OpCodes.Ret));
+                set.Parameters.Add(new ParameterDefinition("value", ParameterAttributes.None, mTestContextType));
+                set.SemanticsAttributes = MethodSemanticsAttributes.Setter;
+                mTargetType.Methods.Add(set);
+                return set;
+            }
         }
     }
 }
